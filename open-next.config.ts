974cbd9df86345974aadcf7642e757f692ca2568
giovannijecha <100841799+GiovanniJ17@users.noreply.github.com>
2025-11-08 22:17:05 +0100

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,10 +1,10 @@
-import { OpenNextConfig } from "@opennextjs/cloudflare";
+import type { OpenNextConfig } from "@opennextjs/cloudflare";
 
 const config: OpenNextConfig = {
   default: {
-    compatibilityDate: "2025-11-08",
-    compatibilityFlags: ["nodejs_compat"],
-  },
+    includeFiles: ["**/*"],
+    enableCacheInterception: false
+  }
 };
 
 export default config;
